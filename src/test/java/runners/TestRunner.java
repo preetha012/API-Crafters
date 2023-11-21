71bc4eff94731ccea22b7577009d958fc6865af2
unknown <manjula.kla8@gmail.com>
2023-11-21 10:34:18 -0500

    ruunercommit

--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -5,24 +5,16 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
-//import org.testng.annotations.DataProvider;
-//import io.cucumber.testng.AbstractTestNGCucumberTests;
-//import io.cucumber.testng.CucumberOptions;
-
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
 			plugin = {"pretty", "html:target/DieticianAPI_Cucumber.html",
 					"json:target/cucumber.json",
-
-					//"junit:target/cucumber-reports/Cucumber.xml",
 					"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:",
 					"io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"
 					}, //reporting purpose
 			monochrome=false,  //console output color
-			features = {"src/test/resources/features/01DieticianUserLogin.feature",
-						"src/test/resources/features/03DieticianRole.feature",
-						"src/test/resources/features/05Logout.feature"
+			features = {"src/test/resources/features"
 					
 					}, //location of feature files		
 
@@ -30,4 +22,5 @@ import io.cucumber.junit.CucumberOptions;
 		)
 public class TestRunner {
 	
+
 }
