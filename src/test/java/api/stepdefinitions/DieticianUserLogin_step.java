44b37088f7967cfa8a51dbdbd6f4eb8a0244d7bb
unknown <manjula.kla8@gmail.com>
2023-11-21 01:36:24 -0500

    final commit

--- src/test/java/api/stepdefinitions/DieticianUserLogin_step.java
+++ src/test/java/api/stepdefinitions/DieticianUserLogin_step.java
@@ -22,51 +22,6 @@ import io.restassured.specification.RequestSpecification;
 
 public class DieticianUserLogin_step {
 	
-	RequestSpecification request;
-	Response response;
-	ValidatableResponse valid_resp;
-	public static UserRequestBody userRequestbody;
-	public static UserPayload userpayload;
-	public static String Dietician_token;
-	
-	@Given("User creates POST Request for login as Dietician.")
-	public void user_creates_post_request_with_fields_and_from_excel() {
-	    
-		 RestAssured.baseURI = Routes.base_url;
-	        this.request = RestAssured
-	        		.given().log().all()
-	        		.header("Content-Type", "application/json");
-		  		
-	}
-
-	@When("User as dietician sends request Body with mandatory , additional fields with {string} and {string} from excel.")
-	public void user_sends_request_body_with_mandatory_additional_fields(String KeyOption, String sheetname) throws Exception {
-	   
-		
-			userpayload = userRequestbody.PostUserBody(KeyOption,sheetname);
-		
-	    	  System.out.println("------"+Routes.login_Url);
-	    	  response = request.body(userpayload).post(Routes.login_Url);
-	      
-	      }	
-
-	@Then("User as dietician receives Status with response body.")
-	public void user_receives_status_with_response_body_and_from_excel() {
-	    
-		 valid_resp = response.then().log().all()
-				 		.assertThat().statusCode(200)
-				 		.body("token", Matchers.notNullValue());
-		// Dietician_token = response.body().asString();
-		 Dietician_token = response.body().path("token");
-		 System.out.println("Token ----->>>"+Dietician_token);
-				 		
-	}
-
-/*	public  String getToken() {
-		Dietician_token = response.body().path("token");
-		 System.out.println("Token ----->>>"+Dietician_token);
-		 return Dietician_token;
-	} */
 	
 	
 	
