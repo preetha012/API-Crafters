80e18f376d65163b063034e1e7eab4d125fe06be
preetha012 <preetha012@gmail.com>
2023-11-21 02:25:48 -0500

    featureDieticianChanges

--- src/test/java/api/com/Hooks.java
+++
@@ -1,5 +0,0 @@
-package api.com;
-
-public class Hooks {
-
-}
--- src/test/java/runners/TestRunner.java
+++ src/test/java/runners/TestRunner.java
@@ -4,9 +4,6 @@ import org.junit.runner.RunWith;
 
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-//import org.testng.annotations.DataProvider;
-//import io.cucumber.testng.AbstractTestNGCucumberTests;
-//import io.cucumber.testng.CucumberOptions;
 
 
 @RunWith(Cucumber.class)
@@ -28,10 +25,5 @@ import io.cucumber.junit.CucumberOptions;
 		)
 public class TestRunner {
 	
-	//@Override
-    //@DataProvider(parallel = false)
-    //public Object[][] scenarios() {
-				
-		//return super.scenarios();
 
 }
